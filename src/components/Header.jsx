e6cd5b7dd220fa6ffd795db805dcae58629c4d9b
unknown <abdohani362@gmail.com>
2024-02-07 00:53:24 +0200

    fixed nav menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { IoPersonSharp } from "react-icons/io5";
 import logo from '../assets/logo.svg'
@@ -11,7 +11,24 @@ import { FaPeopleCarry } from "react-icons/fa";
 
 function Header() {
   const [isOpen, setIsOpen] = useState(false)
-  const location = useLocation();
+  
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+      function handleClickOutside(event) {
+          if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+              setIsOpen(false);
+          }
+      }
+  
+      // Bind the event listener
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+          // Unbind the event listener on clean up
+          document.removeEventListener("mousedown", handleClickOutside);
+      };
+  }, []);
+
   return (
     <div  className='bg-secondaryDarkLead/30 w-full py-3 md:py-7 px-5 md:px-20 flex flex-row justify-between gap-52 items-center relative z-10'>
       <img src={logo} alt="logo" width={60} />
@@ -32,10 +49,10 @@ function Header() {
         </span>
         {
           isOpen && 
-          <div className="absolute z-[99999999] top-20 right-10 bg-secondaryDarkLead p-4 rounded-md flex flex-col gap-4">
-            <Link to={'/'} className="text-secondary text-2xl flex gap-2 items-center" ><MdOutlineInsertChart /> Reports</Link>
-            <Link to={'/customers'} className="text-secondary text-2xl flex gap-2 items-center" ><FaPeopleCarry />Customers</Link>
-            <Link to={'/orders'} className="text-secondary text-2xl flex gap-2 items-center" ><PiPackageFill />orders</Link>
+          <div ref={dropdownRef} className="absolute z-[99999999] top-20 right-10 bg-secondaryDarkLead p-4 rounded-md flex flex-col gap-4">
+            <Link to={'/'} className="text-secondary text-2xl flex gap-2 items-center" onClick={()=> setIsOpen(!isOpen)} ><MdOutlineInsertChart /> Reports</Link>
+            <Link to={'/customers'} className="text-secondary text-2xl flex gap-2 items-center" onClick={()=> setIsOpen(!isOpen)} ><FaPeopleCarry />Customers</Link>
+            <Link to={'/orders'} className="text-secondary text-2xl flex gap-2 items-center" onClick={()=> setIsOpen(!isOpen)} ><PiPackageFill />orders</Link>
           </div>
         }
       </div>
